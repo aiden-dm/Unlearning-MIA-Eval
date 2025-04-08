909edeb5a8524f9c11ce259acfd1fff54491e1ce
Aiden Drover-Mattinen <adrovermattinen@gmail.com>
2025-04-08 15:11:27 -0400

    Adding my validation logic to the scrub model.

--- Final_Structure/scrub.py
+++ Final_Structure/scrub.py
@@ -10,6 +10,8 @@ import torch
 # Adding necessary paths to the system path
 sys.path.append('/content/Unlearning-MIA-Eval')
 
+from Final_Structure.evaluate import train_validation
+
 # Imports from the SCRUB repository
 from Third_Party_Code.SCRUB.thirdparty.repdistiller.distiller_zoo import DistillKL
 from Third_Party_Code.SCRUB.thirdparty.repdistiller.helper.loops import train_distill, validate
@@ -17,10 +19,10 @@ from Third_Party_Code.SCRUB.thirdparty.repdistiller.helper.loops import train_di
 def scrub(model, loaders, args):
 
     # Extracting the loaders that we want
-    valid_loader = loaders[1]
     train_forget_loader = loaders[3]
     train_retain_loader = loaders[4]
     valid_forget_loader = loaders[5]
+    valid_retain_loader = loaders[6]
 
     # Defining hyperparameters
     kd_T = args.kd_T
@@ -52,7 +54,12 @@ def scrub(model, loaders, args):
     module_list.append(model_t)
 
     # Track accuracy
-    acc_rs, acc_fs, acc_vs, acc_fvs = [], [], [], []
+    tf_accs = []
+    tr_accs = []
+    vf_accs = []
+    vr_accs = []
+    losses = []
+    epoch_list = []
 
     # Define validate args
     v_opt = SimpleNamespace()
@@ -71,52 +78,38 @@ def scrub(model, loaders, args):
     for epoch in range(1, sgda_epochs + 1):
         #lr = sgda_adjust_learning_rate(epoch, args_f, optimizer)
 
-        # Validate on retained and forgotten data
-        acc_r, _, _ = validate(train_retain_loader, model_s, criterion_cls, v_opt, True)
-        acc_f, _, _ = validate(train_forget_loader, model_s, criterion_cls, v_opt, True)
-        acc_v, _, _ = validate(valid_loader, model_s, criterion_cls, v_opt, True)
-        acc_fv, _, _ = validate(valid_forget_loader, model_s, criterion_cls, v_opt, True)
-
-        # Storing accuracy results
-        acc_rs.append(100 - acc_r.item())
-        acc_fs.append(100 - acc_f.item())
-        acc_vs.append(100 - acc_v.item())
-        acc_fvs.append(100-acc_fv.item())
-
         # Train model
         maximize_loss = 0
         if epoch <= msteps:
             maximize_loss = train_distill(epoch, train_forget_loader, module_list, None, criterion_list, optimizer, t_opt, "maximize")
         train_acc, train_loss = train_distill(epoch, train_retain_loader, module_list, None, criterion_list, optimizer, t_opt, "minimize")
 
+        losses.append(train_loss)
+        epoch_list.append(epoch)
+        acc_dict = train_validation(model, 
+                                    train_retain_loader, 
+                                    train_forget_loader, 
+                                    valid_retain_loader, 
+                                    valid_forget_loader)
+        tr_accs.append(acc_dict['tr_acc'])
+        tf_accs.append(acc_dict['tf_acc'])
+        vr_accs.append(acc_dict['vr_acc'])
+        vf_accs.append(acc_dict['vf_acc'])
+
         # Print checkpoint progress
         print(f"Epoch {epoch}: maximize loss: {maximize_loss:.2f}, minimize loss: {train_loss:.2f}, train_acc: {train_acc}")
-    
-    # Saving final accuracies after training
-    acc_r, _, _ = validate(train_retain_loader, model_s, criterion_cls, t_opt, True)
-    acc_f, _, _ = validate(train_forget_loader, model_s, criterion_cls, t_opt, True)
-    acc_v, _, _ = validate(valid_loader, model_s, criterion_cls, t_opt, True)
-    acc_fv, _, _ = validate(valid_forget_loader, model_s, criterion_cls, t_opt, True)
-    acc_rs.append(100 - acc_r.item())
-    acc_fs.append(100 - acc_f.item())
-    acc_vs.append(100-acc_v.item())
-    acc_fvs.append(100-acc_fv.item())
 
-    # Plotting results
-    indices = list(range(0,len(acc_rs)))
-    plt.plot(indices, acc_rs, marker='*', color=u'#1f77b4', alpha=1, label='retain-set')
-    plt.plot(indices, acc_fs, marker='o', color=u'#ff7f0e', alpha=1, label='forget-set')
-    plt.plot(indices, acc_vs, marker='^', color=u'#2ca02c',alpha=1, label='validation-set')
-    plt.plot(indices, acc_fvs, marker='.', color='red',alpha=1, label='forget-validation-set')
-    plt.legend(prop={'size': 14})
-    plt.tick_params(labelsize=12)
-    plt.xlabel('epoch',size=14)
-    plt.ylabel('error',size=14)
-    plt.grid()
-    plt.savefig('/content/Unlearning-MIA-Eval/Final_Structure/checkpoints/scrub_unlearning_accuracy_plot.png', bbox_inches='tight')
-    
     # Save a copy of the student model to use in evaluation
     save_path = "/content/Unlearning-MIA-Eval/Final_Structure/checkpoints/scrub_applied.pt"
     torch.save(model_s.state_dict(), save_path)
 
-    return model_s
+    history = {
+        'losses': losses,
+        'epoch_list': epoch_list,
+        'tr_accs': tr_accs,
+        'tf_accs': tf_accs,
+        'vr_accs': vr_accs,
+        'vf_accs': vf_accs
+    }
+
+    return model_s, history
