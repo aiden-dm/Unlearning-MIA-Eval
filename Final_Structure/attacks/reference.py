bd9371b1d5d619dac0687b55b96074db6ce3fa2f
Bhavya Chavali <chavab@rpi.edu>
2025-09-29 12:49:52 -0400

    MIA Files
    Committing MIA Files to test in colab

---
+++ Final_Structure/attacks/reference.py
@@ -0,0 +1,57 @@
+import sys
+import torch
+from torch.utils.data import ConcatDataset
+
+sys.path.append('/content/Unlearning-MIA-Eval')
+
+from Final_Structure.training import load_model
+
+from Third_Party_Code.MIADisparity.miae.attacks.reference_mia import ReferenceAuxiliaryInfo, ReferenceAttack, ReferenceModelAccess
+from Third_Party_Code.MIADisparity.experiment.models import ResNet
+
+def get_custom_resnet(dataset):
+    num_classes = 10 if dataset == "cifar10" else 100
+    num_blocks = [2, 2, 2, 2]                              # ResNet18
+    input_size = 32                                        # CIFAR image size
+
+    return ResNet(num_blocks=num_blocks, num_classes=num_classes, input_size=input_size).to('cuda')
+
+def reference_mia(target_model_path, loaders, config):
+    train_retain_loader = loaders['train_retain_loader']
+    train_forget_loader = loaders['train_forget_loader']
+    valid_forget_loader = loaders['valid_forget_loader']
+    test_forget_loader = loaders['test_forget_loader']
+
+    aux_dataset = train_retain_loader.dataset
+
+    target_model = load_model("cifar10", target_model_path)
+    untrained_model = get_custom_resnet("cifar10")
+
+    model_access = ReferenceModelAccess(
+        model = target_model,
+        untrained_model= untrained_model
+    )
+
+    aux_info = ReferenceAuxiliaryInfo(config)
+    attack = ReferenceAttack(model_access, aux_info)
+
+    # Prepare the attack with retained data (members)
+    attack.prepare(aux_dataset)
+
+    unseen_dataset = ConcatDataset([valid_forget_loader.dataset, test_forget_loader.dataset])
+
+    forget_scores = attack.infer(train_forget_loader.dataset)
+    retain_scores = attack.infer(train_retain_loader.dataset)
+    unseen_scores = attack.infer(unseen_dataset)
+    
+    return {
+        "forget_scores": forget_scores,
+        "retain_scores": retain_scores,
+        "unseen_scores": unseen_scores,
+        "threshold": attack.threshold,
+        "aux_info": aux_info
+    }
+    
+
+
+
---
+++ Final_Structure/attacks/yeom.py
@@ -0,0 +1,63 @@
+# Imports
+import sys
+import torch
+from torch.utils.data import ConcatDataset
+
+# Adding necessary paths to the system path
+sys.path.append('/content/Unlearning-MIA-Eval')
+
+# Framework imports
+from Final_Structure.training import load_model
+
+# Third party imports
+from Third_Party_Code.MIADisparity.miae.attacks.yeom_mia import YeomAttack, YeomModelAccess, YeomAuxiliaryInfo
+from Third_Party_Code.MIADisparity.experiment.models import ResNet
+
+def get_custom_resnet(dataset):
+    num_classes = 10 if dataset == "cifar10" else 100
+    num_blocks = [2, 2, 2, 2]                              # ResNet18
+    input_size = 32                                        # CIFAR image size
+
+    return ResNet(num_blocks=num_blocks, num_classes=num_classes, input_size=input_size).to('cuda')
+
+def yeom_mia(target_model_path, loaders, config):
+
+    train_retain_loader = loaders['train_retain_loader']
+    train_forget_loader = loaders['train_forget_loader']
+    valid_forget_loader = loaders['valid_forget_loader']
+    test_forget_loader = loaders['test_forget_loader']
+
+    aux_dataset = train_retain_loader.dataset
+
+    target_model = load_model("cifar10", target_model_path)
+    untrained_model = get_custom_resnet("cifar10")
+    
+    model_access = YeomModelAccess(
+        model=target_model,
+        untrained_model=untrained_model
+    )
+
+    aux_info = YeomAuxiliaryInfo(config)
+    attack = YeomAttack(model_access, aux_info)
+    
+    # Prepare the attack with retained data (members)
+    attack.prepare(aux_dataset)
+
+    unseen_dataset = ConcatDataset([valid_forget_loader.dataset, test_forget_loader.dataset])
+
+    forget_scores = attack.infer(train_forget_loader.dataset)
+    retain_scores = attack.infer(train_retain_loader.dataset)
+    unseen_scores = attack.infer(unseen_dataset)
+    
+    return {
+        "forget_scores": forget_scores,
+        "retain_scores": retain_scores,
+        "unseen_scores": unseen_scores,
+        "threshold": attack.threshold,
+        "aux_info": aux_info
+    }
+
+
+
+    
+    
